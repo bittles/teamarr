37abde2525906273973e0b8eac765d4f88a0e38b
bittles <bauern.sc@gmail.com>
2025-12-10 04:31:11 -0600

    change date short to m/d isntead of mmm dd

--- epg/template_engine.py
+++ epg/template_engine.py
@@ -277,7 +277,8 @@ class TemplateEngine:
                 local_datetime = game_datetime.astimezone(ZoneInfo(epg_timezone))
 
                 variables['game_date'] = local_datetime.strftime('%A, %B %d, %Y')
-                variables['game_date_short'] = local_datetime.strftime('%b %d')
+                # variables['game_date_short'] = local_datetime.strftime('%b %d')
+                variables['game_date_short'] = local_datetime.strftime('%-m/%-d')
 
                 # Use user's time format preferences for game_time
                 if time_format_settings:
